be315b26f21a8ebcc0e56b1a89b8b2e87c74aab9
rebekajakob <rebusacka@gmail.com>
2022-11-15 11:09:26 +0100

    add constructors for Table class

--- src/main/java/com/coodecool/pickyourspot/model/Table.java
+++ src/main/java/com/coodecool/pickyourspot/model/Table.java
@@ -1,6 +1,7 @@
 package com.coodecool.pickyourspot.model;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -9,5 +10,19 @@ public class Table {
     String name;
     String address;
     Map<LocalDateTime,UUID> reservations;
+
+    public Table(UUID id, String name, String address, Map<LocalDateTime, UUID> reservations) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.reservations = reservations;
+    }
+
+    public Table(String name, String address) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.address = address;
+        this.reservations = new HashMap<>();
+    }
 
 }
