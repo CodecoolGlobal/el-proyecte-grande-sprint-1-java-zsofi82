d5b0fe30dbd896d8b72f4fc2a76892f3fc754cf2
zsofi82 <zsofigaal@gmail.com>
2022-11-15 11:47:18 +0100

    update TableController with additional routes

--- src/main/java/com/coodecool/pickyourspot/controller/TableController.java
+++ src/main/java/com/coodecool/pickyourspot/controller/TableController.java
@@ -3,8 +3,7 @@ package com.coodecool.pickyourspot.controller;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/")
@@ -14,5 +13,34 @@ public class TableController {
     public String index() {
         return "index";
     }
+
+    @GetMapping(value = "map")
+    public String displayMap() {
+        return "map";
+    }
+
+    @GetMapping(value= "registration")
+    public String displayRegistration() {
+        return "registration";
+    }
+
+    //TODO: add user
+    @PostMapping(value = "registration")
+    public String handleRegistration(@RequestBody User user) {
+        return "redirect:";
+    }
+
+    @GetMapping(value= "login")
+    public String displayLogin() {
+        return "login";
+    }
+
+    //TODO: login user create session
+    @PostMapping(value = "login")
+    public String handleLogin(@RequestBody User user) {
+        return "redirect:";
+    }
+
+
 
 }
