7a3922db00cbf971ebc927563b024701e57810d3
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-11-15 09:59:19 +0100

    :tada: add starter files

---
+++ src/main/java/com/coodecool/pickyourspot/PickYourSpotApplication.java
@@ -0,0 +1,13 @@
+package com.coodecool.pickyourspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickYourSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickYourSpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coodecool/pickyourspot/PickYourSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coodecool.pickyourspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickYourSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
