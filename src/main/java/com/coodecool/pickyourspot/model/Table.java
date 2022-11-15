92870bbbb75c2ee2b4cbd4094698c9164de7fd79
rebekajakob <rebusacka@gmail.com>
2022-11-15 11:08:03 +0100

    add fields for Table class

--- src/main/java/com/coodecool/pickyourspot/model/Table.java
+++ src/main/java/com/coodecool/pickyourspot/model/Table.java
@@ -1,4 +1,13 @@
 package com.coodecool.pickyourspot.model;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
 public class Table {
+    UUID id;
+    String name;
+    String address;
+    Map<LocalDateTime,UUID> reservations;
+
 }
