fd5bff2c12f0be0e16a448bb6e2cfaade112b38e
Botond <atabboti@gmail.com>
2022-11-16 14:18:57 +0100

    add service test file with empty test methods

---
+++ src/test/java/com/coodecool/pickyourspot/service/ProductServiceTest.java
@@ -0,0 +1,40 @@
+package com.coodecool.pickyourspot.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class ProductServiceTest {
+
+    ProductService service;
+
+
+    @Test
+    void getAllTables() {
+
+    }
+
+    @Test
+    void addNewUser() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void addNewTable() {
+    }
+
+    @Test
+    void getTableById() {
+    }
+
+    @Test
+    void addReservation() {
+    }
+}
