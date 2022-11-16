3d9b8c8422c0d75eef4a0cbf1a3a90895eb96370
rebekajakob <rebusacka@gmail.com>
2022-11-16 10:36:46 +0100

    implement getUserByID in APIController

--- src/main/java/com/coodecool/pickyourspot/controller/APIController.java
+++ src/main/java/com/coodecool/pickyourspot/controller/APIController.java
@@ -1,16 +1,22 @@
 package com.coodecool.pickyourspot.controller;
 
 import com.coodecool.pickyourspot.model.User;
+import com.coodecool.pickyourspot.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api")
 public class APIController {
+    @Autowired
+    ProductService productService;
     @GetMapping("/get-user/{id}")
-    public User getUserById(@PathVariable String id){
-        return null;
+    public Optional<User> getUserById(@PathVariable String id){
+        return productService.getUserById(id);
     }
 }
