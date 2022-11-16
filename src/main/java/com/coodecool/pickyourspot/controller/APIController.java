957525562a5cbec514f9bb3a9879771457215686
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-11-16 11:05:54 +0100

    implement get all users api route

--- src/main/java/com/coodecool/pickyourspot/controller/APIController.java
+++ src/main/java/com/coodecool/pickyourspot/controller/APIController.java
@@ -8,16 +8,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class APIController {
-    @Autowired
     ProductService productService;
+
+    @Autowired
+    public APIController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/user/{id}")
     public Optional<User> getUserById(@PathVariable String id){
         return productService.getUserById(id);
+    }
+
+    @GetMapping("/user/all")
+    public List<User> getAllUsers(){
+        return productService.getAllUsers();
     }
 
 
