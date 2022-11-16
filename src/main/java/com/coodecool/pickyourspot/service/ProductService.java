73af1d0d285d32412d1d94326efa1991c7c25dd9
rebekajakob <rebusacka@gmail.com>
2022-11-16 10:36:34 +0100

    implement getUserByID in ProductService

--- src/main/java/com/coodecool/pickyourspot/service/ProductService.java
+++ src/main/java/com/coodecool/pickyourspot/service/ProductService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Component
 public class ProductService {
@@ -28,5 +30,9 @@ public class ProductService {
     public void addNewUser(User user) {
         userDao.addUser(user);
         System.out.println(userDao.getAllUsers());
+    }
+
+    public Optional<User> getUserById(String id){
+        return userDao.getUserById(UUID.fromString(id));
     }
 }
