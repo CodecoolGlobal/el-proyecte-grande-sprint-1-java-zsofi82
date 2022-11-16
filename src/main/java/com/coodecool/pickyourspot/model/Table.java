6a23cc1453b49b02a6b3ebb57323e1bf6c278b72
rebekajakob <rebusacka@gmail.com>
2022-11-16 11:16:47 +0100

    comment out one of the Table constructors

--- src/main/java/com/coodecool/pickyourspot/model/Table.java
+++ src/main/java/com/coodecool/pickyourspot/model/Table.java
@@ -11,14 +11,14 @@ public class Table {
     String name;
     String address;
     Map<LocalDateTime,UUID> reservations;
-
+/*
     public Table(UUID id, String name, String address, Map<LocalDateTime, UUID> reservations) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.reservations = reservations;
     }
-
+*/
     public Table(String name, String address) {
         this.id = UUID.randomUUID();
         this.name = name;
