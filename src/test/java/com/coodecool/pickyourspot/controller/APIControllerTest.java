45789f3ddbdf25dbc62a4bd8496b343d26fbe78d
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-11-16 12:40:58 +0100

    Add more test to unit tests

--- src/test/java/com/coodecool/pickyourspot/controller/APIControllerTest.java
+++ src/test/java/com/coodecool/pickyourspot/controller/APIControllerTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -15,16 +17,25 @@ class APIControllerTest {
 
     @Test
     void getUserByValidId() {
-
         User sentUser = new User("username", "email", "password");
-        String userId = sentUser.getId().toString();
         productService.addNewUser(sentUser);
+        String userId = sentUser.getId().toString();
 
         User foundUser = productService.getUserById(userId).get();
-        System.out.println(foundUser);
 
         assertEquals(sentUser, foundUser);
+    }
 
+    @Test
+    void getUserByInvalidId() {
+        User sentUser = new User("username", "email", "password");
+        productService.addNewUser(sentUser);
+        sentUser.setId(UUID.randomUUID());
+        String userId = sentUser.getId().toString();
+
+        User foundUser = productService.getUserById(userId).get();
+
+        assertNotEquals(sentUser, foundUser);
     }
 
 }
