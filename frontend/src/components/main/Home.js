4129a88c049ee83107f222d6764d7607e207c910
rebekajakob <rebusacka@gmail.com>
2022-11-29 13:25:17 +0100

    add sample tables

--- frontend/src/components/main/Home.js
+++ frontend/src/components/main/Home.js
@@ -1,9 +1,37 @@
 import Footer from "../Footer";
+import Tables from "./Tables";
 
 const Home = () => {
+    const tables = [{
+        id: "647478383",
+        name: "My favourite table",
+        address: "1189, Béla utca 6",
+        reservations: null
+    }, {
+        id: "647474545483",
+        name: "My Macko table",
+        address: "1109, Buli utca 23",
+        reservations: null
+    }, {
+        id: "64747232383",
+        name: "My tables",
+        address: "1234, Béla utca 33",
+        reservations: null
+    }, {
+        id: "64747232383",
+        name: "My tables",
+        address: "1234, Béla utca 33",
+        reservations: null
+    }, {
+        id: "64747232383",
+        name: "My tables",
+        address: "1234, Béla utca 33",
+        reservations: null
+    }]
   return (
     <div>Home
-      <Footer/>
+        <Tables tables={tables}/>
+      <Footer />
     </div>
 
   )
