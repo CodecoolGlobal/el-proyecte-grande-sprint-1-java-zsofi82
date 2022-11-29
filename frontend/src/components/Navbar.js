4ed011d13886b8ba7419ee64fb1d0f064bbc1f46
Botond <atabboti@gmail.com>
2022-11-29 14:59:16 +0100

    add about link

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,11 +9,13 @@ const Navbar = () => {
     <nav className="navbar sticky-top navbar-expand-lg bg-dark text-light">
       <div class="container-fluid">
         <span class="navbar-brand mb-0 h1 text-light" onClick={() => { navigate("/") }}>Pick Your Spot</span>
-        <ul className="flex-row-reverse navbar-nav">
+        <ul className="flex-row navbar-nav">
           <li className="nav-item col m-1">
             <Button text={"Register"} onClick={() => { navigate("/registration") }}></Button></li>
           <li className="nav-item col m-1">
             <Button text={"Login"} onClick={() => { navigate("/login") }}></Button></li>
+          <li className="nav-item col m-1">
+            <Button text={"About"} bootstrapClassname={"btn-link"} onClick={() => { navigate("/about") }} ></Button></li>
         </ul>
       </div>
     </nav>
