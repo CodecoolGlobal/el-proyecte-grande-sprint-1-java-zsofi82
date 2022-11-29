7e9266b5d1b0e953a0c62ec11237349faa1ef68c
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-11-29 14:56:05 +0100

    Implement Login React Component, refactor Login

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,7 +1,16 @@
+import Footer from "../Footer"
+import Navbar from "../Navbar"
+import LoginForm from "./LoginForm"
+import "./Login.css"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="loginContainer">
+      <Navbar />
+      <h1>Login:</h1>
+      <LoginForm />
+      <Footer />
+    </div>
   )
 }
 
--- src/main/java/com/coodecool/pickyourspot/controller/ApiController.java
+++ src/main/java/com/coodecool/pickyourspot/controller/ApiController.java
@@ -10,8 +10,21 @@ public class ApiController {
     @PostMapping("/registration")
     public void registerUser(@RequestBody AppUser appuser) {
 
+        // TODO validate if username exists
+        // TODO hash password
         // TODO implement storing user data
-        
+
         System.out.println(appuser);
     }
+
+    @PostMapping("/login")
+    public void loginUser(@RequestBody AppUser appuser) {
+
+        // TODO implement storing user data
+        // TODO implement logging in
+        System.out.println(appuser);
+    }
+
+
+
 }
