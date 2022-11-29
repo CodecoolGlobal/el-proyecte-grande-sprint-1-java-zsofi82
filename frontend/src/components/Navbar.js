1e9151693be632c3be95f1a2b02af14026293eee
Botond <atabboti@gmail.com>
2022-11-29 14:32:03 +0100

    position buttons next to each other

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,12 +2,14 @@ import Button from "./reusable_elements/Button"
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark text-light">
+    <nav className="navbar sticky-top navbar-expand-lg bg-dark text-light">
       <div class="container-fluid">
         <span class="navbar-brand mb-0 h1 text-light">Pick Your Spot</span>
-        <ul className="d-flex">
-          <li><Button text={"Register"}></Button></li>
-          <li><Button text={"Login"}></Button></li>
+        <ul className="flex-row-reverse navbar-nav">
+          <li className="nav-item col">
+            <Button text={"Register"}></Button></li>
+          <li className="nav-item col">
+            <Button text={"Login"}></Button></li>
         </ul>
       </div>
     </nav>
