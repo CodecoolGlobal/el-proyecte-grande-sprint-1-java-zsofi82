1f8107b926e729129c078e90a6dbf31c06a5d7e5
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-11-29 14:14:08 +0100

    Implement registration Component, no css yet

--- frontend/src/components/registration/RegistrationForm.js
+++ frontend/src/components/registration/RegistrationForm.js
@@ -1,23 +1,49 @@
+import { useEffect, useState } from "react"
 
 const RegistrationForm = () => {
+    const [userData, setUserData] = useState()
 
     function grabFormData(e) {
         e.preventDefault()
-        const userName = e.target['registrationText'].value
-        const userPassword = e.target['registrationPassword'].value
-
-        console.log(`name: ${userName}, psw: ${userPassword}`)
+        const userName = e.target['name'].value
+        const userPassword = e.target['password'].value
+        const userEmail = e.target['email'].value
+        const data = { "username": userName, "password": userPassword, "email": userEmail }
+        const update = () => { setUserData(data) }
+        update()
     }
+
+    useEffect(() => {
+        if (userData) {
+            try {
+                const backendUrl = `/api/registration`
+                fetch(backendUrl, {
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json',
+                    },
+                    body: JSON.stringify(userData)
+                })
+            } catch (err) {
+                console.error(err)
+            }
+        }
+
+
+    }, [userData])
 
     return (
         <>
             <div className="registrationFormDiv">
                 <form className="registrationForm" onSubmit={(e) => grabFormData(e)}>
                     <label>Name:</label>
-                    <input type="text" name="registrationText">
+                    <input type="text" name="name">
                     </input>
                     <label>Password:</label>
-                    <input type="password" name="registrationPassword">
+                    <input type="password" name="password">
+                    </input>
+                    <label>Email:</label>
+                    <input type="email" name="email">
                     </input>
                     <button type='submit'>Submit</button>
                 </form>
---
+++ src/main/java/com/coodecool/pickyourspot/controller/ApiController.java
@@ -0,0 +1,17 @@
+package com.coodecool.pickyourspot.controller;
+
+import com.coodecool.pickyourspot.model.AppUser;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @PostMapping("/registration")
+    public void registerUser(@RequestBody AppUser appuser) {
+
+        // TODO implement storing user data
+        
+        System.out.println(appuser);
+    }
+}
