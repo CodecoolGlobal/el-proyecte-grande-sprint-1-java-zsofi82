2c08d24d464e8eb36ca5c702f435b25e279f3901
rebekajakob <rebusacka@gmail.com>
2022-11-29 14:51:52 +0100

    replace tables list to server response

--- frontend/src/components/main/Home.js
+++ frontend/src/components/main/Home.js
@@ -1,40 +1,40 @@
 import Footer from "../Footer";
 import Tables from "./Tables";
+import {useEffect, useState} from "react";
 
-const Home = () => {
-    const tables = [{
-        id: "647478383",
-        name: "My favourite table",
-        address: "1189, Béla utca 6",
-        reservations: null
-    }, {
-        id: "647474545483",
-        name: "My Macko table",
-        address: "1109, Buli utca 23",
-        reservations: null
-    }, {
-        id: "64747232383",
-        name: "My tables",
-        address: "1234, Béla utca 33",
-        reservations: null
-    }, {
-        id: "64747232383",
-        name: "My tables",
-        address: "1234, Béla utca 33",
-        reservations: null
-    }, {
-        id: "64747232383",
-        name: "My tables",
-        address: "1234, Béla utca 33",
-        reservations: null
-    }]
-  return (
-    <div>Home
-        <Tables tables={tables}/>
-      <Footer />
-    </div>
+const Home =  () => {
+    const [tableData, setTableData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  )
+    useEffect(() => {
+        fetch("api/table")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setLoading(false)
+                    setTableData(result)
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setLoading(false);
+                    setError(error);
+                }
+            )
+    }, [])
+    if(loading == false){
+        return (
+            <div>Home
+                <p>{tableData.length? <Tables  tables={tableData}/> : 'No tables to show' }</p>
+
+                <Footer/>
+            </div>
+
+        )
+    }
+
 }
 
 export default Home
