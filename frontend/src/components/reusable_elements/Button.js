ce4f1f81ff1f6381a160153ca1768af5e100afae
Botond <atabboti@gmail.com>
2022-11-29 11:25:18 +0100

    add the text instide the element,  make a default onClick event

--- frontend/src/components/reusable_elements/Button.js
+++ frontend/src/components/reusable_elements/Button.js
@@ -1,18 +1,20 @@
 import PropTypes from "prop-types";
 
-const Button = ({onClick, text, bootstrapClassname}) => {
+const Button = ({ onClick, text, bootstrapClassname }) => {
   return (
-    <button 
-    className={["btn", bootstrapClassname]}
-    onClick={onClick} 
-    text={text}
-    style={{backgroundColor: color}}
-    ></button>
+    <button
+      className={["btn", bootstrapClassname]}
+      onClick={onClick}
+    >
+      {text}
+    </button>
   )
 }
 // https://getbootstrap.com/docs/5.2/components/buttons/
 Button.defaultProps = {
   bootstrapClassname: "btn-primary",
+  text: "Button",
+  onClick: (e) => { console.log("Pressed: " + e.target) }
 };
 
 Button.propTypes = {
