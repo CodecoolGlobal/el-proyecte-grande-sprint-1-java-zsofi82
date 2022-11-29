3fa4e89c2df241b98a4e9cc1d50ce6f88bf7a8ba
rebekajakob <rebusacka@gmail.com>
2022-11-29 13:36:01 +0100

    add blue background to Table

--- frontend/src/components/main/Table.js
+++ frontend/src/components/main/Table.js
@@ -1,7 +1,7 @@
 const Table = ({table}) => {
     return(
         <div className="col" >
-            <div className="card" >
+            <div className="card bg-info" >
                 <div className="card-body">
                     <h5 className="card-title">{table.name}</h5>
                     <p className="card-text"> {table.address} </p>
