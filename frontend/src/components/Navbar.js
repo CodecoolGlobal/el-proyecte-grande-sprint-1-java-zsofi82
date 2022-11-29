d8bf1b17a91657663c3f3c3e061f86696deef493
Botond <atabboti@gmail.com>
2022-11-29 14:40:30 +0100

    add routing to buttons

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import Button from "./reusable_elements/Button"
 
+
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className="navbar sticky-top navbar-expand-lg bg-dark text-light">
       <div class="container-fluid">
         <span class="navbar-brand mb-0 h1 text-light">Pick Your Spot</span>
         <ul className="flex-row-reverse navbar-nav">
           <li className="nav-item col m-1">
-            <Button text={"Register"} ></Button></li>
+            <Button text={"Register"} onClick={()=>{navigate("/login")}}></Button></li>
           <li className="nav-item col m-1">
-            <Button text={"Login"}></Button></li>
+            <Button text={"Login"} onClick={()=>{navigate("/login")}}></Button></li>
         </ul>
       </div>
     </nav>
