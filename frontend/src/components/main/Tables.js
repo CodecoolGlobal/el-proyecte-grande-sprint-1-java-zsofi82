52f9ce8e7b6934017b6aef851d19f070b0fab9e7
rebekajakob <rebusacka@gmail.com>
2022-11-29 14:51:25 +0100

    add key to Table

--- frontend/src/components/main/Tables.js
+++ frontend/src/components/main/Tables.js
@@ -4,7 +4,7 @@ const Tables = ({tables}) => {
     return(
         <div className={"row row-cols-1 row-cols-md-3 g-4"} style={{padding: "2em"}}>
             {
-                tables.map((table)=> (<Table table={table}/> ))
+                tables.map((table)=> (<Table key={table.id} table={table}/> ))
             }
         </div>
     )
