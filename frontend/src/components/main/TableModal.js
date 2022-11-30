d76dab5e82569f3ade3ff000dfde1f991ddbca72
Botond <atabboti@gmail.com>
2022-11-30 12:00:17 +0100

    add exit button

--- frontend/src/components/main/TableModal.js
+++ frontend/src/components/main/TableModal.js
@@ -1,10 +1,18 @@
 
 const TableModal = ({ table, onExit }) => {
   return (
-    <div>
-      <p onClick={() => { onExit() }}>X</p>
-      <p>{table.reservations}</p>
-      {console.log(table)}
+    <div className="card">
+      <div className="card-body">
+        <div className="card-title d-flex flex-row">
+          <h5 className="card-title col">asd</h5>
+          
+          <div className="btn-close col" onClick={() => { onExit() }} >
+            
+          </div>
+        </div>
+        <p>asd</p>
+        {console.log(table)}
+      </div>
     </div>
   )
 }
