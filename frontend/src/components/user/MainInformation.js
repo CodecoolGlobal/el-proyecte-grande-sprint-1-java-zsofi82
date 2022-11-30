7d76424a2a027d51237ebf6dc2b06b51e0a998cd
rebekajakob <rebusacka@gmail.com>
2022-11-30 12:20:33 +0100

    show username and email

--- frontend/src/components/user/MainInformation.js
+++ frontend/src/components/user/MainInformation.js
@@ -1,6 +1,9 @@
-const MainInformation = () => {
+const MainInformation = ({user}) => {
     return(
-        <div></div>
+        <div>
+            <h1> Hello {user.username}! </h1>
+            <h3> Your email: {user.email}</h3>
+        </div>
     )
 }
 export default MainInformation
