b621cc63718d7c2818a6a8bbacfbe5368b617a52
rebekajakob <rebusacka@gmail.com>
2022-11-30 11:03:54 +0100

    create User.js and delete duplication

--- frontend/src/components/User.js
+++
@@ -1,8 +0,0 @@
-
-const User = () => {
-  return (
-    <div>User</div>
-  )
-}
-
-export default User
---
+++ frontend/src/components/user/User.js
@@ -0,0 +1,13 @@
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+
+const User = () => {
+    return(
+        <div>
+            <Navbar/>
+            <Footer/>
+        </div>
+    )
+
+}
+export default User
