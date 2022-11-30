a7062cba3b5902e4071c6dc442e362127665fc3e
rebekajakob <rebusacka@gmail.com>
2022-11-30 12:20:59 +0100

    get userdata by id from url

--- frontend/src/components/user/User.js
+++ frontend/src/components/user/User.js
@@ -1,15 +1,34 @@
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import {Route, Link, Routes, useParams} from 'react-router-dom';
+import {useEffect, useState} from "react";
+import MainInformation from "./MainInformation";
 
 const User = () => {
+    const [userData, setUserData] = useState({});
+    const [tableData, setTableData] = useState({});
+    const [reservationData, setReservationData] = useState({});
     const params = useParams();
-    console.log(params.userId);
+    const userId = params.userId
+
+
+    useEffect(()=> {
+        async function fetchUser() {
+            let res = await fetch(`/api/user/${userId}`)
+            let data = await res.json()
+            setUserData(data)
+        }
+        try {
+            fetchUser()
+        } catch(err) {
+            console.error(err)
+        }
+    })
 
     return(
         <div>
             <Navbar/>
-            <p>User</p>
+            <MainInformation user={userData} />
             <Footer/>
         </div>
     )
