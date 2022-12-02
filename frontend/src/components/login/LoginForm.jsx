36d895dbe1af24bb2f515542208c3eb074dea767
Bence Fabian <begnoc@gmail.com>
2022-12-02 11:21:15 +0100

    [WIP] some refactoring for login form

--- frontend/src/components/login/LoginForm.jsx
+++ frontend/src/components/login/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Navigate } from "react-router-dom"
 import Button from "../reusable_elements/Button"
 
 const LoginForm = ({ loggedIn, setLoggedIn, setUserName, setUserId }) => {
@@ -19,52 +19,51 @@ const LoginForm = ({ loggedIn, setLoggedIn, setUserName, setUserId }) => {
 
     useEffect(() => {
         if (dataToServer) {
-            try {
-                const backendUrl = `/api/login`
-                fetch(backendUrl, {
-                    method: 'POST',
-                    headers: {
-                        'Content-type': 'application/json',
-                    },
-                    body: JSON.stringify(dataToServer)
+            const backendUrl = `/api/login`
+            fetch(backendUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(dataToServer)
+            })
+                .then(res => {
+                    setRawResponse(res)
+                    return res
                 })
-                    .then(res => {
+                .then(res => res.json())
+                .then(res => {
+                    if (res.status === 200) {
+                        sessionStorage.setItem("username", serverResponse.username)
+                        sessionStorage.setItem("userid", serverResponse.id)
+                        setDataToServer(null)
                         setRawResponse(res)
-                        return res
-                    })
-                    .then(res => res.json())
-                    .then(res => setServerResponse(res))
-                if (serverResponse  && rawResponse.status === 200) {
-                    sessionStorage.setItem("username", serverResponse.username)
-                    sessionStorage.setItem("userid", serverResponse.id)
-                    setDataToServer(null)
-                    setRawResponse(null)
-                    navigate("/")
-                }
-            } catch (err) {
-                console.error(err)
-            }
+                        navigate("/")
+                    }
+                    setServerResponse(res);
+                })
+                .catch(err => console.error(err))
         }
     }, [dataToServer, serverResponse, navigate])
 
 
 
-    return (
-        <>
-            <div className="loginFormDiv">
-                <form className="loginForm" onSubmit={(e) => grabFormData(e)}>
-                    <label>Name:</label>
-                    <input type="text" name="name" required>
-                    </input>
-                    <label>Password:</label>
-                    <input type="password" name="password" required>
-                    </input>
-                    <Button type='submit' text='Submit' />
-                    <div>{(serverResponse && rawResponse.status === 400) && "Username or password incorrect!" }</div>
-                </form>
-            </div>
-        </>
-    )
+    return (loggedIn ?
+            <Navigate to={'/'} /> :
+                <>
+                    <div className="loginFormDiv">
+                        <form className="loginForm" onSubmit={(e) => grabFormData(e)}>
+                            <label>Name:</label>
+                            <input type="text" name="name" required>
+                            </input>
+                            <label>Password:</label>
+                            <input type="password" name="password" required>
+                            </input>
+                            <Button type='submit' text='Submit' />
+                            <div>{(serverResponse && rawResponse.status === 400) && "Username or password incorrect!" }</div>
+                        </form>
+                    </div>
+                </>);
 }
 
 export default LoginForm
