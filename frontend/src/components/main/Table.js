423d51c9726c5b6b55a8c53ec24d194a16ddd2f1
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-12-14 11:12:30 +0100

    Add embedded map to card

--- frontend/src/components/main/Table.js
+++ frontend/src/components/main/Table.js
@@ -1,10 +1,20 @@
 const Table = ({ table, showDetails }) => {
+
+    const qParam = 'Budapest, Zichy utca 1'
+    const API_KEY = process.env.REACT_APP_GMAP_API_KEY
+    const zoom = '17'
+
+    const url = `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${qParam}&zoom=${zoom}`
+
+
     return (
         <div className="col" onClick={() => { showDetails(table) }}>
             <div className="card bg-info" >
                 <div className="card-body modal-style">
                     <h5 className="card-title">{table.name}</h5>
                     <p className="card-text"> {table.address} </p>
+                    <iframe className="myMap" loading="lazy" allowFullScreen title='test'
+                        src={url}></iframe>
                 </div>
             </div>
         </div>
--- frontend/src/components/user/ReservedTables.js
+++ frontend/src/components/user/ReservedTables.js
@@ -1,12 +1,12 @@
 import Reservations from "./Reservations";
 
-const ReservedTables = ({tables, setDelete}) => {
-    return(
+const ReservedTables = ({ tables, setDelete }) => {
+    return (
         <div>
-        <h4 className="text-left display-4" style={{marginTop:"2em"}}>Your reservations: </h4>
-        <div className={"row row-cols-1 row-cols-md-3 g-4"} style={{ padding: "2em", marginRight: "0px" }}>
-            {tables.map((table) => (<Reservations key={table.id} table={table} setDelete={setDelete} />))}
-        </div>
+            <h4 className="text-left display-4" style={{ marginTop: "2em" }}>Your reservations: </h4>
+            <div className={"row row-cols-1 row-cols-md-3 g-4"} style={{ padding: "2em", marginRight: "0px" }}>
+                {tables.map((table) => (<Reservations key={table.id} table={table} setDelete={setDelete} />))}
+            </div>
         </div>
     )
 }
