c554cc8461e31be4491dff84351186dfb99fe335
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-12-14 13:16:08 +0100

    Refactor Map asa a component

--- frontend/src/components/main/Table.js
+++ frontend/src/components/main/Table.js
@@ -1,20 +1,13 @@
+import EmbeddedMap from "../reusable_elements/EmbeddedMap"
+
 const Table = ({ table, showDetails }) => {
-
-    const qParam = 'Budapest, Zichy utca 1'
-    const API_KEY = process.env.REACT_APP_GMAP_API_KEY
-    const zoom = '17'
-
-    const url = `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${table.name}&zoom=${zoom}`
-
-
     return (
         <div className="tables-col col" onClick={() => { showDetails(table) }}>
             <div className="card tables-card bg-info" >
                 <div className="card-body tables-card-body modal-style">
                     <h5 className="card-title">{table.name}</h5>
                     <p className="card-text"> {table.address} </p>
-                    <iframe className="myMap" loading="lazy" allowFullScreen title='test'
-                        src={url}></iframe>
+                    <EmbeddedMap className={"mainPageMap"} source={table.name}/>
                 </div>
             </div>
         </div>
