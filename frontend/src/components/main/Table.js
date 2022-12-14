c9c00bd4f9a82e2f1ddd9b99a75940cda40aaea1
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-12-14 11:31:09 +0100

    Add style to table cards and maps

--- frontend/src/components/main/Table.js
+++ frontend/src/components/main/Table.js
@@ -4,13 +4,13 @@ const Table = ({ table, showDetails }) => {
     const API_KEY = process.env.REACT_APP_GMAP_API_KEY
     const zoom = '17'
 
-    const url = `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${qParam}&zoom=${zoom}`
+    const url = `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${table.name}&zoom=${zoom}`
 
 
     return (
-        <div className="col" onClick={() => { showDetails(table) }}>
-            <div className="card bg-info" >
-                <div className="card-body modal-style">
+        <div className="tables-col col" onClick={() => { showDetails(table) }}>
+            <div className="card tables-card bg-info" >
+                <div className="card-body tables-card-body modal-style">
                     <h5 className="card-title">{table.name}</h5>
                     <p className="card-text"> {table.address} </p>
                     <iframe className="myMap" loading="lazy" allowFullScreen title='test'
