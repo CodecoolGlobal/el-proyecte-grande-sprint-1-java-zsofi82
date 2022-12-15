6490eb74f84f3792e25599c7bf949a5ac5dfe0df
zsofi82 <zsofigaal@gmail.com>
2022-12-15 13:29:59 +0100

    Clean AppUser class

--- src/main/java/com/coodecool/pickyourspot/model/AppUser.java
+++ src/main/java/com/coodecool/pickyourspot/model/AppUser.java
@@ -1,15 +1,8 @@
 package com.coodecool.pickyourspot.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
-import org.hibernate.annotations.GeneratorType;
-import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
-import java.beans.Transient;
-import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -29,35 +22,10 @@ public class AppUser {
 
     private String password;
 
-//    @OneToMany
-//    private List<Reservation> reservations;
-
-//    public AppUser(){
-//    }
-
-//    @JsonCreator
-//    public AppUser(String username, String email, String password) {
-//        this.username = username;
-//        this.email = email;
-//        var hashedPassword = hash(password);
-//        this.password = hashedPassword;
-//    }
-/*
-    public User(UUID id, String username, String email, String hashedPassword) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = hashedPassword;
-    }
-*/
-
     private String hash(String password) {
         // TODO: remove this eventually
         return password;
     }
-//    public String getUsername() {
-//        return username;
-//    }
 
     @Override
     public boolean equals(Object o) {
@@ -82,35 +50,4 @@ public class AppUser {
                 '}';
     }
 
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    @JsonIgnore
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    @JsonIgnore
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setId(UUID id) {
-//        this.id = id;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    @JsonProperty
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    @JsonProperty
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
 }
