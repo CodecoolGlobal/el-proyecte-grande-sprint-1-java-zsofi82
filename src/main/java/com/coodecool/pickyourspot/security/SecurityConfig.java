ca243322e65f321d0800c6c8f79aa48e14445b9e
rebekajakob <rebusacka@gmail.com>
2023-01-09 14:38:34 +0100

    add SecurityConfig class, EnableWebSecurity annotation and implement a basic configuration

---
+++ src/main/java/com/coodecool/pickyourspot/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.coodecool.pickyourspot.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login", "/registration").permitAll();
+    }
+}
