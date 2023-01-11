b578f4386ece25d86e98b9401d827531be3595ff
Botond <atabboti@gmail.com>
2023-01-11 14:08:26 +0100

    change table modal to use the context token add get tables to the permitted routes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import Button from "./reusable_elements/Button"
 import {useContext} from "react";
 import {TokenContext} from "../App";
@@ -6,40 +6,58 @@ import jwtDecode from "jwt-decode";
 
 
 const Navbar = () => {
-  const {token, setToken} = useContext(TokenContext)
-  const navigate = useNavigate();
+    const {token, setToken} = useContext(TokenContext)
+    const navigate = useNavigate();
 
-  const decodedToken = jwtDecode(token)
-  const username = decodedToken.sub
-  function clearLocalStorage()  {
-    localStorage.clear()
-    setToken(null)
-  }
+    function clearLocalStorage() {
+        localStorage.clear()
+        setToken(null)
+    }
 
-  return (
-    <nav className="navbar sticky-top">
-      <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1 backToHome" onClick={() => { navigate("/") }}>Pick Your Spot</span>
-        <ul className="flex-row navbar-nav">
-          {!token && <li className="nav-item col navbarButton">
-            <Button bootstrapClassname={"navbar-button-style"} text={"Register"} onClick={() => { navigate("/registration") }}></Button>
-          </li>}
-          {!token && <li className="nav-item col navbarButton" >
-            <Button bootstrapClassname={"navbar-button-style"} text={"Login"} onClick={() => { navigate("/login") }}></Button>
-          </li>}
-          {token && <li className="nav-item col navbarButton">
-            <Button bootstrapClassname={"navbar-button-style"} text={"Profile"} classname={"btn-link"} className="navbar-button-style" onClick={() => { navigate(`/user/${username}`) }} ></Button>
-          </li>}
-          {token && <li className="nav-item col navbarButton">
-            <Button bootstrapClassname={"navbar-button-style"} text={"Logout"} classname={"btn-link"} onClick={() => { clearLocalStorage() }} ></Button>
-          </li>}
-          <li className="nav-item col navbarButton">
-            <Button bootstrapClassname={"navbar-button-style"} text={"About"} classname={"btn-link"} onClick={() => { navigate("/about") }} ></Button>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  )
+    function parseUsername(token) {
+        const decodedToken = jwtDecode(token)
+        return decodedToken.sub
+    }
+
+    return (
+        <nav className="navbar sticky-top">
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1 backToHome" onClick={() => {
+                    navigate("/")
+                }}>Pick Your Spot</span>
+                <ul className="flex-row navbar-nav">
+                    {!token && <li className="nav-item col navbarButton">
+                        <Button bootstrapClassname={"navbar-button-style"} text={"Register"} onClick={() => {
+                            navigate("/registration")
+                        }}></Button>
+                    </li>}
+                    {!token && <li className="nav-item col navbarButton">
+                        <Button bootstrapClassname={"navbar-button-style"} text={"Login"} onClick={() => {
+                            navigate("/login")
+                        }}></Button>
+                    </li>}
+                    {token && <li className="nav-item col navbarButton">
+                        <Button bootstrapClassname={"navbar-button-style"} text={"Profile"} classname={"btn-link"}
+                                className="navbar-button-style" onClick={() => {
+                            navigate(`/user/${parseUsername(token)}`)
+                        }}></Button>
+                    </li>}
+                    {token && <li className="nav-item col navbarButton">
+                        <Button bootstrapClassname={"navbar-button-style"} text={"Logout"} classname={"btn-link"}
+                                onClick={() => {
+                                    clearLocalStorage()
+                                }}></Button>
+                    </li>}
+                    <li className="nav-item col navbarButton">
+                        <Button bootstrapClassname={"navbar-button-style"} text={"About"} classname={"btn-link"}
+                                onClick={() => {
+                                    navigate("/about")
+                                }}></Button>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
