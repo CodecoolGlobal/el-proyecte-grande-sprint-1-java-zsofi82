696cfee5abe2860a21f0934a3c0e8a188a75ead0
Botond <atabboti@gmail.com>
2023-01-11 14:18:12 +0100

    add token to DeleteReservation.js fetch

--- frontend/src/components/user/DeleteReservation.js
+++ frontend/src/components/user/DeleteReservation.js
@@ -1,9 +1,17 @@
+import {useContext} from "react";
+import {TokenContext} from "../../App";
+import jwtDecode from "jwt-decode";
+
 const DeleteReservation = ({tableId, reservation, setDelete}) => {
+    const {token} = useContext(TokenContext)
+
     async function fetchDeleteReservation() {
         await fetch(`/api/table/${tableId}/reservation`, {
             method: 'DELETE',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+
             },
             body: JSON.stringify(reservation)
         })
