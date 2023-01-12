57e9a95d5313200793a0134617304b24c69e853a
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2023-01-12 15:04:25 +0100

    Refactor login

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,30 +1,22 @@
-
 import LoginForm from "./LoginForm"
 import "./Login.css"
-import { useNavigate } from "react-router-dom"
-import {useContext} from "react";
-import {TokenContext} from "../../App";
+import { Navigate } from "react-router-dom"
+import { useContext } from "react";
+import { TokenContext } from "../../App";
 
 const Login = () => {
-    const navigate = useNavigate()
-    const {token} = useContext(TokenContext)
-
-
-    function getCorrectRoute() {
-        if (token) {
-            navigate("/")
-        } else {
-            return (
-            <div className="loginContainer">
-                <h1>Login:</h1>
-                <LoginForm />
-            </div>)
-        }
-    }
+    const { token } = useContext(TokenContext)
 
     return (
         <>
-            {getCorrectRoute()}
+            {token ?
+                <Navigate to="/" replace />
+                :
+                <div className="loginContainer">
+                    <h1>Login:</h1>
+                    <LoginForm />
+                </div>
+            }
         </>
     )
 }
