50e8eb3c8db4cc9224c12038a3aed13429ee8a1b
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2023-01-12 18:04:59 +0100

    Add placeholder text to user page if there are no reservations

--- frontend/src/components/user/ProfilePageContent.js
+++ frontend/src/components/user/ProfilePageContent.js
@@ -2,12 +2,16 @@ import Reservations from "./Reservations";
 
 const ProfilePageContent = ({ tables, setDelete }) => {
     return (
-        <div>
+        <>
             <h4 className="text-left display-4" style={{ marginTop: "2em" }}>Your reservations: </h4>
-            <div className={"row row-cols-1 row-cols-md-3 g-4"} style={{ padding: "2em", marginRight: "0px" }}>
-                {tables.map((table) => (<Reservations key={table.id} table={table} setDelete={setDelete} />))}
-            </div>
-        </div>
+            {tables.length === 0 ?
+                <div>There are no reservations</div>
+                :
+                <div className={"row row-cols-1 row-cols-md-3 g-4"} style={{ padding: "2em", marginRight: "0px" }}>
+                    {tables.map((table) => (<Reservations key={table.id} table={table} setDelete={setDelete} />))}
+                </div>
+            }
+        </>
     )
 }
 export default ProfilePageContent
